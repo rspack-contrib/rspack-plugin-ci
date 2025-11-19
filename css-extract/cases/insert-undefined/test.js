22c1abb1643978dec6095adecf747769ac9bd138
LingyuCoder <lingyucoder@gmail.com>
2025-11-19 16:29:33 +0800

    feat: support prefetch and preload in css extract plugin

--- css-extract/cases/insert-undefined/test.js
+++ css-extract/cases/insert-undefined/test.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 module.exports = function (outputDirectory, _stats) {
 	const mainContent = fs.readFileSync(path.resolve(outputDirectory, "main.js"), "utf8");
-	expect(mainContent).toContain(`if (oldTag) {
-  oldTag.parentNode.insertBefore(linkTag, oldTag.nextSibling);
-} else {
-  document.head.appendChild(linkTag);
-}`);
+	expect(mainContent).toContain(`if (oldTag) {`);
+  expect(mainContent).toContain(`oldTag.parentNode.insertBefore(linkTag, oldTag.nextSibling);`);
+  expect(mainContent).toContain(`} else {`);
+  expect(mainContent).toContain(`document.head.appendChild(linkTag);`);
+  expect(mainContent).toContain(`}`);
 };
